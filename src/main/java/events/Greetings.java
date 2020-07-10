c559350567662f46632b2b66c78e56bb7008b9b7
RodrigoLimaM <digo100200@gmail.com>
2020-07-10 19:50:19 -0300

    adding greetings log

--- src/main/java/events/Greetings.java
+++ src/main/java/events/Greetings.java
@@ -1,12 +1,16 @@
 package events;
 
+import commands.Help;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import java.awt.*;
+import java.util.logging.Logger;
 
 public class Greetings extends ListenerAdapter {
+
+    private static final Logger LOGGER = Logger.getLogger(Help.class.getName());
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         String messageSent = event.getMessage().getContentRaw();
@@ -17,16 +21,22 @@ public class Greetings extends ListenerAdapter {
         if (messageSent.toUpperCase().contains("BOA NOITE") && !event.getAuthor().isBot()){
             eb.setDescription("Boa Noite! :new_moon_with_face:");
             event.getChannel().sendMessage(eb.build()).queue();
+
+            LOGGER.info("Message: " +messageSent);
         }
 
         if (messageSent.toUpperCase().contains("BOA TARDE") && !event.getAuthor().isBot()){
             eb.setDescription("Bom Tarde! :sun_with_face:");
             event.getChannel().sendMessage(eb.build()).queue();
+
+            LOGGER.info("Message: " +messageSent);
         }
 
         if (messageSent.toUpperCase().contains("BOM DIA") && !event.getAuthor().isBot()){
             eb.setDescription("Bom Dia! :partly_sunny:");
             event.getChannel().sendMessage(eb.build()).queue();
+
+            LOGGER.info("Message: " +messageSent);
         }
     }
 }
