9dc6ba0c338a8eee40514150633ecd54af5de059
RodrigoLimaM <digo100200@gmail.com>
2020-07-11 00:13:51 -0300

    refactoring greetings

--- src/main/java/events/Greetings.java
+++ src/main/java/events/Greetings.java
@@ -1,6 +1,5 @@
 package events;
 
-import commands.Help;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -10,33 +9,37 @@ import java.util.logging.Logger;
 
 public class Greetings extends ListenerAdapter {
 
-    private static final Logger LOGGER = Logger.getLogger(Help.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(Greetings.class.getName());
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         String messageSent = event.getMessage().getContentRaw();
 
-        EmbedBuilder eb = new EmbedBuilder()
-                .setColor(new Color(0x8b008b));;
+        EmbedBuilder embedBuilder = new EmbedBuilder()
+                .setColor(new Color(0x8b008b));
 
-        if (messageSent.toUpperCase().contains("BOA NOITE") && !event.getAuthor().isBot()){
-            eb.setDescription("Boa Noite! :new_moon_with_face:");
-            event.getChannel().sendMessage(eb.build()).queue();
-
-            LOGGER.info("Message: " +messageSent);
+        if (!event.getAuthor().isBot() && isGreetingMessage(messageSent, "BOA NOITE")){
+            embedBuilder.setDescription("Boa Noite! :new_moon_with_face:");
+            sendMessageToChannel(embedBuilder, messageSent, event);
         }
 
-        if (messageSent.toUpperCase().contains("BOA TARDE") && !event.getAuthor().isBot()){
-            eb.setDescription("Bom Tarde! :sun_with_face:");
-            event.getChannel().sendMessage(eb.build()).queue();
-
-            LOGGER.info("Message: " +messageSent);
+        if (!event.getAuthor().isBot() && isGreetingMessage(messageSent, "BOA TARDE")){
+            embedBuilder.setDescription("Boa Tarde! :sun_with_face:");
+            sendMessageToChannel(embedBuilder, messageSent, event);
         }
 
-        if (messageSent.toUpperCase().contains("BOM DIA") && !event.getAuthor().isBot()){
-            eb.setDescription("Bom Dia! :partly_sunny:");
-            event.getChannel().sendMessage(eb.build()).queue();
-
-            LOGGER.info("Message: " +messageSent);
+        if (!event.getAuthor().isBot() && isGreetingMessage(messageSent, "BOM DIA")){
+            embedBuilder.setDescription("Bom Dia! :partly_sunny:");
+            sendMessageToChannel(embedBuilder, messageSent, event);
         }
+    }
+
+    private boolean isGreetingMessage(String messageSent, String greetingMessage) {
+        return messageSent.toUpperCase().contains(greetingMessage);
+    }
+
+    private void sendMessageToChannel(EmbedBuilder embedBuilder, String messageSent, GuildMessageReceivedEvent event) {
+        event.getChannel().sendMessage(embedBuilder.build()).queue();
+
+        LOGGER.info("Message: " +messageSent);
     }
 }
