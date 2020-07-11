45fba0895ad9e5a1c78ccf007970376e017e2898
RodrigoLimaM <digo100200@gmail.com>
2020-07-10 23:35:32 -0300

    adding watching status

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
 import commands.Draw;
 import commands.Help;
 import events.Greetings;
+import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        JDA jda = new JDABuilder(System.getenv("BOT_KEY")).build();
+        JDA jda = new JDABuilder(AccountType.BOT)
+                .setToken(System.getenv("BOT_KEY"))
+                .setGame(Game.watching("Jojo Bizarres Adventures"))
+                .build();
 
         jda.addEventListener(new Draw());
         jda.addEventListener(new Help());
